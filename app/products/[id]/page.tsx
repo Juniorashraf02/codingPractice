f42444eb6e0c65a50fea847d32a12f9789c84b24
juniorashraf02 <shuvo.istiak11@gmail.com>
2026-04-05 15:42:46 +0600

    fix: return code added

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductPage() {
+  return (
+    <div>
+      <h1>Product Details</h1>
+      <p>Here you can view the details of a specific product.</p>
+    </div>
+  );
+}
